cafdafeb619357c42fdd095887a6a106ebc9938a
Kumar Pulkit <kumarpulkit734@gmail.com>
2026-07-28 18:10:38 +0000

    Add fourth activity

--- src/app.py
+++ src/app.py
@@ -38,6 +38,12 @@ activities = {
         "schedule": "Mondays, Wednesdays, Fridays, 2:00 PM - 3:00 PM",
         "max_participants": 30,
         "participants": ["john@mergington.edu", "olivia@mergington.edu"]
+    },
+    "Chess Club": {
+        "description": "Learn and play chess",
+        "schedule": "Wednesdays at 5 PM",
+        "max_participants": 12,
+        "participants": []
     }
 }
 
